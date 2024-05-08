e4c2fe56bb602797f34343a6576158fead1442db
Piotr Tobiasz <49806746+tobias-piotr@users.noreply.github.com>
2024-05-08 09:58:38 +0200

    Add losers queue (#7)

--- src/config/celery.py
+++ src/config/celery.py
@@ -1,11 +1,16 @@
 import os
 
 from celery import Celery, signals
+from kombu import Queue
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.config.settings.prod")
 
 app = Celery("waltz")
 app.config_from_object("django.conf:settings", namespace="CELERY")
+app.conf.task_queues = (
+    Queue("celery"),
+    Queue("losers"),
+)
 app.autodiscover_tasks()
 
 
